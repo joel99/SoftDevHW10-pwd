3ed08eb58fbfa243fba4dc2af0fc6b5e3687d0d6
Joel Ye <jye5@stuy.edu>
2017-04-24 14:37:00 -0400

    final

--- pwchkr.py
+++ pwchkr.py
@@ -1,3 +1,5 @@
+import sys
+
 def threshCheck(pw):
     upperList = "QAZWSXEDCRFVTGBYHNUJMIKOLP"
     lowerList = "qazwsxedcrfvtgbyhnujmikolp"
@@ -11,14 +13,30 @@ def strRating(pw):
     lowerList = "qazwsxedcrfvtgbyhnujmikolp"
     if not threshCheck(pw):
         return 0
-    score = 0
+    score = len(pw) + len([x for x in pw if x in symbols])
     processed = [0 if x in symbols else (1 if x in upperList else 2 if x in lowerList else 3) for x in pw]
     lastStatus = processed[0]
+    chain = 0
     for index,x in enumerate(processed):
         if index != 0 and processed[index] == lastStatus:
-            score -= 1
+            chain += 1
         else:
             score += 1
+            if chain >= 2:
+                score -= chain / 10
+            chain = 1
         lastStatus = processed[index]
+    final = score/1000 * 3
+    if 0 in processed:
+        final += 3
+    if 3 in processed:
+        final += 2
+    if 1 in processed:
+        final += 1
+    if 2 in processed:
+        final += 2
+    return final
 
-    
+print "Enter password"
+line = raw_input('> ')
+print "Strength of " + line + ": " + str(strRating(line))
